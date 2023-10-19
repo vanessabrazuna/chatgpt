7b2cd644705aa61afbb0ba166a662d839f7473d0
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2023-10-19 20:43:57 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'ChatGPT Clone',
+  title: 'UI ChatGPT',
 }
 
 export default function RootLayout({
