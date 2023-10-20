b789a831c7569d0fc2d72f79e964b6e6a9e7f039
Vanessa Brazuna <73675022+vanessabrazuna@users.noreply.github.com>
2023-10-19 23:00:28 -0300

    Atualizar o ChatPlaceholder.tsx

--- src/components/ChatPlaceholder.tsx
+++ src/components/ChatPlaceholder.tsx
@@ -3,7 +3,7 @@ import { AlertTriangle, Sun, Zap } from 'lucide-react'
 export function ChatPlaceholder() {
   return (
     <div className="m-5">
-      <h3 className="text-4xl font-bold text-center mt-28 mb-16 text-white">ChatGPT Clone</h3>
+      <h3 className="text-4xl font-bold text-center mt-28 mb-16 text-white">ChatGPT</h3>
       <div className="flex flex-col md:flex-row gap-5 m-auto mb-8 md:max-w-4xl">
 
         <div>
